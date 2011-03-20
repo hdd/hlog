7850aafaa025bd1126fe3d2f57ebc2d2d1c2a9d6
Lorenzo [hdd] Angeli <lorenzo_angeli@yahoo.it>
2011-03-20 19:46:30 +0000

    gmail error handler

--- src/hlog/hlib.py
+++ src/hlog/hlib.py
@@ -1,29 +1,5 @@
 import logging
 
-class InfoFilter(logging.Filter):
-    def filter(self, rec):
-        return rec.levelno == logging.INFO
-
-class DebugFilter(logging.Filter):
-    def filter(self, rec):
-        return rec.levelno == logging.DEBUG
-
-class WarningFilter(logging.Filter):
-    def filter(self, rec):
-        return rec.levelno == logging.WARNING
-    
-class ErrorFilter(logging.Filter):
-    def filter(self, rec):
-        return rec.levelno == logging.ERROR    
-
-class CriticalFilter(logging.Filter):
-    def filter(self, rec):
-        return rec.levelno == logging.CRITICAL      
-
-class NotsetFilter(logging.Filter):
-    def filter(self, rec):
-        return rec.levelno == logging.NOTSET
-       
 class ConsoleErrorHandler(logging.StreamHandler):
     def __init__(self, *args, **kwargs):
         logging.StreamHandler.__init__(self, *args, **kwargs)
@@ -32,8 +8,7 @@ class ConsoleInfoHandler(logging.StreamHandler):
     def __init__(self, *args, **kwargs):
         logging.StreamHandler.__init__(self, *args, **kwargs)
 
-
-class TlsSMTPHandler(logging.handlers.SMTPHandler):
+class GMAIL_SMTPHandler(logging.handlers.SMTPHandler):
     def emit(self, record):
         """
         Emit a record.
