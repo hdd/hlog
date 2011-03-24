4d7224ed88f14b4f31a64a95c064d30b92394bc5
Lorenzo [hdd] Angeli <lorenzo_angeli@yahoo.it>
2011-03-24 22:51:09 +0000

    set LOG_CONFIG_PATH for log config search

--- src/hlog/__init__.py
+++ src/hlog/__init__.py
@@ -1,20 +1,43 @@
+'''
+this library collects all the configuration aspect of the logger
+
+
+'''
 import sys, os
 import logging
 import logging.config
 import inspect
 
-config = 'hConf.config'
-cur_folder=os.path.join(os.path.dirname(__file__),config)
-logging.config.fileConfig(cur_folder)
+__all__ = ["get_config_path","debug","info","error","warning","DEBUGMODE"]
+
+#    default config file included
+_default_config_name = 'hConf.config'
+
+def get_config_path():
+    '''
+    search 
+    '''
+    if "LOG_CONFIG" in os.environ.keys():
+        return os.environ["LOG_CONFIG_PATH"]
+    else:
+        logging.warning("LOG_CONFIG environment variable not found")
+        cur_folder=os.path.dirname(__file__)
+        logging.debug("lookging into local folder %s"%cur_folder)
+        return cur_folder
+    
+    raise Exception , "No Config File Found, please set LOG_CONFIG_PATH"
+
+#    setup the config file
+config_file_path = os.path.join(get_config_path(),_default_config_name)
+logging.config.fileConfig(config_file_path)
 mlog=logging.getLogger("root")
 
 #    check for the DEBUG ENV , if it's set , enable the debug
 DEBUGMODE=os.getenv("DEBUG")
-
 if DEBUGMODE == "1":
-	mlog.warning("<DEBUGMODE ON>")
-	mlog=logging.getLogger("debug")
-	
+    mlog.warning("<DEBUGMODE ON>")
+    mlog=logging.getLogger("debug")
+    
 
 debug = mlog.debug
 info = mlog.info
